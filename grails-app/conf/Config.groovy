e2f64ca8d72ebf2281377c0659bef27a71036357
Rodrigo Andrade <rcaa2@cin.ufpe.br>
2013-08-15 09:19:46 -0300

    config

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -16,7 +16,7 @@ grails {
         //TODO: Before running, make sure that your email login and password appear below
         //TODO: Before committing, make sure that the strings "LOGIN" and "PASSWORD" appear in place of your login and password
         username = "LOGIN"
-        password = "PASSWORD"
+        password = "teste"
         props = ["mail.smtp.auth": "true",
                 "mail.smtp.socketFactory.port": "465",
                 "mail.smtp.socketFactory.class": "javax.net.ssl.SSLSocketFactory",
